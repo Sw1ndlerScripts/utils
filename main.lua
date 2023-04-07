317a4c20b8af166a1d0d85d37530db0c4bb84ec5
Alex <103692349+Sw1ndlerScripts@users.noreply.github.com>
2023-04-06 17:26:27 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -238,3 +238,23 @@ getgenv().antinamesetter = function()
         end)
     ]])
 end
+
+getgenv().serverhop = function()
+    local HttpService = game:GetService("HttpService")
+    
+    local servers = {}
+    local req = syn.request({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100", game.PlaceId)})
+    local body = HttpService:JSONDecode(req.Body)
+    if body and body.data then
+    	for i, v in next, body.data do
+    		if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+    			table.insert(servers, 1, v.id)
+    		end
+    	end
+    end
+    if #servers > 0 then
+    	game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], game.Players.LocalPlayer)
+    else
+    	return warn("Serverhop", "Couldn't find a server.")
+    end
+end
