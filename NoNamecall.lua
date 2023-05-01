700cc212980333f4964f93abca3f551d4f235b35
Alex <103692349+Sw1ndlerScripts@users.noreply.github.com>
2023-04-30 18:13:13 -0700

    Update NoNamecall.lua

--- NoNamecall.lua
+++ NoNamecall.lua
@@ -3,7 +3,7 @@ getgenv().getClosestPlayerNoNamecall = function()
     local closestPlayer = nil
     
     for _, player in pairs(game.Players.GetPlayers(game.Players)) do
-        if player ~= plr and player.Character then
+        if player ~= plr and player.Character and game.FindFirstChild(player.Character, 'HumanoidRootPart') then
             local distance = distanceTo(player.Character.HumanoidRootPart)
             if distance < closestDistance then
                 closestDistance = distance
